fc4ac2ca13298e8051db9672f313ced3db850b61
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-04-11 17:15:02 +0200

    count package (with tests) replaces stopwatch package

--- fetch.go
+++ fetch.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/jreisinger/gosec/stopwatch"
+	"github.com/jreisinger/gosec/count"
 )
 
 func main() {
 	urls := os.Args[1:]
-	sw := stopwatch.Start()
+	start := time.Now()
 	ch := make(chan string)
 	for _, url := range urls {
 		go fetch(url, ch)
@@ -20,20 +20,20 @@ func main() {
 	for range urls {
 		fmt.Println(<-ch)
 	}
-	fmt.Printf("%5.3f elapsed\n", sw.Stop().Seconds())
+	fmt.Printf("%5.3f elapsed\n", time.Since(start).Seconds())
 }
 
 func fetch(url string, ch chan string) {
-	sw := stopwatch.Start()
+	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 	defer resp.Body.Close()
-	n, err := io.Copy(io.Discard, resp.Body)
+	n, err := count.Bytes(resp.Body)
 	if err != nil {
 		log.Print(err)
 	}
-	ch <- fmt.Sprintf("%5.3f %5d %s", sw.Stop().Seconds(), n, url)
+	ch <- fmt.Sprintf("%5.3f %5d %s", time.Since(start).Seconds(), n, url)
 }
