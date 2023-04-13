7a027f37eed996011cbcbb371c855149935624ad
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-04-13 09:42:43 +0200

    don't time urls in fetch to simplify

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -5,12 +5,10 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
 	urls := os.Args[1:]
-	start := time.Now()
 	ch := make(chan string)
 	for _, url := range urls {
 		go fetch(url, ch) // start a goroutine
@@ -18,21 +16,20 @@ func main() {
 	for range urls {
 		fmt.Println(<-ch) // receive from channel
 	}
-	fmt.Printf("%5.3f elapsed\n", time.Since(start).Seconds())
 }
 
 func fetch(url string, ch chan string) {
-	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		ch <- fmt.Sprint(err)
 		return
 	}
 	defer resp.Body.Close()
-	n, err := io.Copy(io.Discard, resp.Body)
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
 	if err != nil {
 		ch <- fmt.Sprint(err)
 		return
 	}
-	ch <- fmt.Sprintf("%5.3f %7d %s", time.Since(start).Seconds(), n, url)
+	ch <- fmt.Sprintf("%-7d %s", nbytes, url)
 }
